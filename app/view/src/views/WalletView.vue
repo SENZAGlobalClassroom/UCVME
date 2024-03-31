1aadf6a343228df74a18cc543b85da90abbb18f6
Elitsa Koleva <53860330+elikookie@users.noreply.github.com>
2024-03-31 15:50:25 +0100

    made Wallet layout better and functional on the frontend

--- app/view/src/views/WalletView.vue
+++ app/view/src/views/WalletView.vue
@@ -3,10 +3,15 @@
         <!-- need to add top bar -->
         <Card class="card-container">
             <template #header>
-                <h3 >Saved CVs</h3>
+                <h3>Saved CVs</h3>
             </template>
             <template #content>
-                <Wallet :collections="collections"></Wallet>
+            <Wallet 
+                :collections="collections" 
+                @add-collection="handleNewCollection" 
+                @update-collection="handleUpdateCollection" 
+                @delete-collection="handleDeleteCollection">
+            </Wallet>
             </template>
         </Card>
         <Navbar></Navbar>
@@ -14,7 +19,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref,onMounted } from 'vue';
 import Wallet from '@/components/Wallet.vue';
 
 //test data imports
@@ -28,7 +33,87 @@ import cat2 from '@/assets/cat2.jpg';
 import cat3 from '@/assets/cat3.jpg';
 import cat4 from '@/assets/cat4.jpg';
 
-// test data
+// funcs to handle emits from Wallet
+const handleNewCollection = (collectionName) => {
+    const newCollection = {
+        id: Date.now(),
+        name: collectionName,
+        cvs: [],
+    };
+    collections.value.push(newCollection);
+};
+
+const handleUpdateCollection = ({ id, name }) => {
+    const collectionIndex = collections.value.findIndex(c => c.id === id);
+    if (collectionIndex !== -1) {
+        collections.value[collectionIndex].name = name;
+    }
+};
+
+const handleDeleteCollection = (id) => {
+    collections.value = collections.value.filter(c => c.id !== id);
+};
+
+// Test data 
+// this should be any CVs that were saved/liked by the user
+const allCVs = ref([
+    {
+        id: 'cv1',
+        name: 'Erica Lee',
+        video: test1,
+        stillImage: cat1,
+        workExperience: [
+            {
+                jobTitle: 'Hairstylist',
+                startDate: '2020-01',
+                endDate: '2021-12',
+                description: 'Hairstyle.',
+                memory: 'A client once gave me a tip',
+                referencePhone: '000000'
+            },
+        ],
+    },
+    {
+        id: 'cv2',
+        name: 'John Doe',
+        video: test2,
+        stillImage: cat2,
+        workExperience: [
+            {
+                jobTitle: 'Gardener',
+                startDate: '2020-01',
+                endDate: '2021-12',
+                description: 'Maintained and beautified gardens.',
+                memory: 'A client once...',
+                referencePhone: '000000'
+            },
+        ],
+    },
+    {
+        id: 'cv3',
+        name: 'Amanda Doe',
+        video: test3,
+        stillImage: cat3,
+        workExperience: [
+            {
+                jobTitle: 'Pet Sitter',
+                startDate: '2020-01',
+                endDate: '2021-12',
+                description: 'Taking care of cats and dogs',
+                memory: 'A client once...',
+                referencePhone: '000000'
+            },
+        ],
+    },
+]);
+
+const allCVsCollection = {
+    id: 0,
+    name: 'All CVs',
+    cvs: allCVs,
+  };
+
+// this should be the collections made by the user with added cvs to it
 const collections = ref([
     {
         id: 1,
@@ -142,130 +227,21 @@ const collections = ref([
                     },
                 ],
             },
-            {
-                id: 'cv2',
-                name: 'John Doe',
-                video: test2,
-                stillImage: cat2,
-                workExperience: [
-                    {
-                        jobTitle: 'Gardener',
-                        startDate: '2020-01',
-                        endDate: '2021-12',
-                        description: 'Maintained and beautified gardens.',
-                        memory: 'A client once...',
-                        referencePhone: '000000'
-                    },
-                ],
-            },
-            {
-                id: 'cv3',
-                name: 'Amanda Doe',
-                video: test3,
-                stillImage: cat3,
-                workExperience: [
-                    {
-                        jobTitle: 'Pet Sitter',
-                        startDate: '2020-01',
-                        endDate: '2021-12',
-                        description: 'Taking care of cats and dogs',
-                        memory: 'A client once...',
-                        referencePhone: '000000'
-                    },
-                ],
-            },
-            {
-                id: 'cv4',
-                name: 'Jenny Smith',
-                video: test4,
-                stillImage: cat4,
-                workExperience: [
-                    {
-                        jobTitle: 'Marketing Expert',
-                        startDate: '2020-01',
-                        endDate: '2021-12',
-                        description: 'Sales and marketing stuff for businesses',
-                        memory: 'A client once...',
-                        referencePhone: '000000'
-                    },
-                ],
-            },
-
         ]
     },
     {
         id: 4,
         name: 'Musician CVs',
         cvs: [
-            {
-                id: 'cv1',
-                name: 'Erica Lee',
-                video: test1,
-                stillImage: cat1,
-                workExperience: [
-                    {
-                        jobTitle: 'Hairstylist',
-                        startDate: '2020-01',
-                        endDate: '2021-12',
-                        description: 'Hairstyle.',
-                        memory: 'A client once gave me a tip',
-                        referencePhone: '000000'
-                    },
-                ],
-            },
-            {
-                id: 'cv2',
-                name: 'John Doe',
-                video: test2,
-                stillImage: cat2,
-                workExperience: [
-                    {
-                        jobTitle: 'Gardener',
-                        startDate: '2020-01',
-                        endDate: '2021-12',
-                        description: 'Maintained and beautified gardens.',
-                        memory: 'A client once...',
-                        referencePhone: '000000'
-                    },
-                ],
-            },
-            {
-                id: 'cv3',
-                name: 'Amanda Doe',
-                video: test3,
-                stillImage: cat3,
-                workExperience: [
-                    {
-                        jobTitle: 'Pet Sitter',
-                        startDate: '2020-01',
-                        endDate: '2021-12',
-                        description: 'Taking care of cats and dogs',
-                        memory: 'A client once...',
-                        referencePhone: '000000'
-                    },
-                ],
-            },
-            {
-                id: 'cv4',
-                name: 'Jenny Smith',
-                video: test4,
-                stillImage: cat4,
-                workExperience: [
-                    {
-                        jobTitle: 'Marketing Expert',
-                        startDate: '2020-01',
-                        endDate: '2021-12',
-                        description: 'Sales and marketing stuff for businesses',
-                        memory: 'A client once...',
-                        referencePhone: '000000'
-                    },
-                ],
-            },
-
         ]
     }
 
 ]);
+
+onMounted(() => {
+    collections.value.unshift(allCVsCollection);
+});
+
 </script>
 
 <style scoped>
