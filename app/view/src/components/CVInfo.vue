2c04b5918b9d7c797365b31e52a89bf3bb2068d7
matemikus <c21379483@mytudublin.ie>
2024-04-24 15:47:10 +0900

    Added mbti and work exp

--- app/view/src/components/CVInfo.vue
+++ app/view/src/components/CVInfo.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="wrapper">
         <div class="header-container">
-        <h1>{{ username }}</h1>
-        <div>
-            <SpeedDial :model="items" direction="down" showIcon="pi pi-ellipsis-v" hideIcon="pi pi-ellipsis-h"
-                :tooltipOptions="{ position: 'left' }" />
+            <div>
+                <h1>{{ username }}</h1>
+                <h4 class="mbti">{{ mbti }}</h4>
+            </div>
+            <div class="speedDial">
+                <SpeedDial :model="items" direction="down" showIcon="pi pi-ellipsis-v" hideIcon="pi pi-ellipsis-h" :tooltipOptions="{ position: 'left' }" />
+            </div>
         </div>
     </div>
     <div class="video-container">
@@ -16,8 +19,12 @@
             {{ aboutMe }}
         </ScrollPanel>
     </div>
+    <div class="work-experience-container">
+        <h1>Work Experience</h1>
+        <ScrollPanel class="scroll-panel">
+            {{ workExperience }}
+        </ScrollPanel>
     </div>
-
 
     <!-- Share CV Popup -->
     <Dialog v-model:visible="sharingDialogue" modal header="Share CV" :style="{ width: '25rem' }">
@@ -57,10 +64,12 @@ const toast = useToast();
 
 // the actual data needs to come from the db (from whatever the user put in the CV Process)
 const aboutMe = `Hi there! I'm Joe, a proud resident of the stunning landscapes of Kerry, where the rolling green hills inspire my passion for farming. With roots deeply embedded in this rich agricultural region, I find joy in cultivating the land and nurturing life from seed to harvest. Whether it's tending to crops or caring for livestock, there's an undeniable connection to the earth that fuels my spirit. When I'm not knee-deep in soil, you'll likely find me exploring the countryside or sharing stories over a cup of tea. Farming isn't just a livelihood for me; it's a way of life that I cherish every day.`;
+const workExperience = "Ever since I was knee-high, I've been mucking about in the dirt and loving every minute of it. Started out at Flynn's Orchards – just a stone's throw away from home in Wicklow. I got real good at figuring out what those apple and pear trees needed to thrive. Picking fruit at dawn, that's the kind of thing that teaches you to wait for the right moment. Next up was Green Hill Dairy. Not just your average farm, but a place that really cared about doing things right and kind. Got my hands dirty with the cows, learned the dance of the milking routine, and all about keeping the fields green and happy. Then there was a stint at Willow Creek, working with a bunch of smart folks trying to fix up tired old fields using nature's playbook. We did everything – mixing up what we planted, giving the land a break, and bringing back birds and bugs. Lately, I've been running the show at Harvest Bounty. It's a big veggie farm where every day is about getting those plants from tiny seeds to someone's dinner plate. It's a lot to keep track of, but I wouldn't have it any other way. ";
 
 // should be videoURL from postgres db
 const videoUrl = "https://firebasestorage.googleapis.com/v0/b/ucvme-131a0.appspot.com/o/Eli.mp4?alt=media&token=0438e70d-651d-4ac1-a8b3-555f0d0625d8"
 var username = 'username'; // Placeholder username
+var mbti = 'mbti';
 
 const token = localStorage.getItem('token');
 var decoded = jwtDecode(token).username;
@@ -137,7 +146,7 @@ const items = ref([
 .wrapper{
     padding-left: 2rem;
     padding-right: 2rem;
-
+    padding: 1rem;
 }
 .center {
     display: flex;
@@ -147,9 +156,19 @@ const items = ref([
 }
 
 .header-container {
-    display: flex;
-    justify-content: space-between;
-    padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  padding-left: 0; /* Adjust this value as needed to align to the red line */
+}
+
+.mbti {
+  margin-top: 0.5rem; /* Add space between the username and mbti */
+}
+
+.speedDial {
+  margin-left: auto; /* This pushes the SpeedDial to the right */
 }
 
 .video-container {
@@ -158,9 +177,11 @@ const items = ref([
 
 
 .work-experience-container {
-    padding: 1.5rem;
-    border-radius:  0px 0px 25px 25px;
+    padding: 2.5rem;
+    border-radius:  25px 25px 25px 25px;
     background-color: #e3e3e367; //should be decided based on the colour the user picked
+    margin-bottom: 1rem;
+    margin-top: 1rem;
 }
 
 .scroll-panel {
@@ -197,10 +218,15 @@ const items = ref([
 @media (max-width: 767px) {
 
     .header-container {
-        max-width: 96dvw;
         display: flex;
-        justify-content: space-between;
+        flex-direction: column; /* Stack vertically */
+        align-items: center; /* Center items horizontally */
         padding: 1rem;
+        text-align: center; /* Align the text to the center for all child elements */
+    }
+
+    .mbti {
+        margin-top: 0.5rem; /* Add space between the username and mbti */
     }
 
     .video-container {
